37d07e5f7755360a73fa13c96db0295e000375b1
_Anderson_Andrade_ <31743641+AndersonAndrad@users.noreply.github.com>
2020-10-13 22:17:46 -0300

    configuring the database

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+// Update with your config settings.
+module.exports = {
+  development: {
+    client: process.env.DB_DIALECT,
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: './src/database/migrations',
+    },
+    useNullAsDefault: true,
+
+    staging: {
+      client: 'postgresql',
+      connection: {
+        database: 'my_db',
+        user: 'username',
+        password: 'password',
+      },
+      pool: {
+        min: 2,
+        max: 10,
+      },
+      migrations: {
+        tableName: 'knex_migrations',
+      },
+    },
+
+    production: {
+      client: 'postgresql',
+      connection: {
+        database: 'my_db',
+        user: 'username',
+        password: 'password',
+      },
+      pool: {
+        min: 2,
+        max: 10,
+      },
+      migrations: {
+        tableName: 'knex_migrations',
+      },
+    },
+  },
+};
